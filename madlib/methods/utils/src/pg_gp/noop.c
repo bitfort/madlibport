a4c6e04c19ff858de58baae9cb4753e7f7a04365
Victor Bittorf <bittorf@cs.wisc.edu>
2013-08-24 13:55:44 -0700

    getting madlib port.

--- madlib/methods/utils/src/pg_gp/noop.c
+++
@@ -1,9 +0,0 @@
-#include <postgres.h>
-#include <fmgr.h>
-
-PG_FUNCTION_INFO_V1(noop);
-Datum
-noop(PG_FUNCTION_ARGS) {
-    (void) fcinfo;
-    PG_RETURN_VOID();
-}
--- madlib/src/modules/sample/sample.hpp
+++
@@ -1,9 +0,0 @@
-/* -----------------------------------------------------------------------------
- *
- * @file sample.hpp
- *
- * @brief Umbrella header that includes all sampling headers
- *
- * -------------------------------------------------------------------------- */
-
-#include "weighted_sample.hpp"
