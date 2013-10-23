e120ca59a032be09204565aea202f963f50c0ef7
cloudera <cloudera@localhost.localdomain>
2013-10-23 13:44:34 -0700

    removed unused madlib files.

--- madlib/deploy/preflight.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# $0 - Script Path, $1 - Package Path, $2 - Target Location, and $3 - Target Volumn
-
-if [ -d "/usr/local/madlib/Versions" ]
-then
-    mv /usr/local/madlib/Versions /usr/local/madlib/Versions.bak
-fi
