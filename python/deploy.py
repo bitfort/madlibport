a5a6c234f783739723d0acfbb27c5315d81ee7a7
cloudera <cloudera@localhost.localdomain>
2013-10-25 10:27:49 -0700

    tested linr predict, fixed sig

--- python/deploy.py
+++ python/deploy.py
@@ -26,7 +26,7 @@ queries = [
     "create aggregate function linr(string, double) returns string location '/user/cloudera/liblinr.so' UPDATE_FN='LinrUpdate';"
 
     "DROP function IF EXISTS linrpredict(string, string);",
-#    "create function linrpredict(string, string) returns boolean location '/user/cloudera/liblinr.so' SYMBOL='LinrPredict';"
+    "create function linrpredict(string, string) returns double location '/user/cloudera/liblinr.so' SYMBOL='LinrPredict';"
 
     #
     # Utilities
